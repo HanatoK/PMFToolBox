8681c5043c2049f2c65c302c1c42a979117bcc32
hanatok <summersnow9403@gmail.com>
2020-09-22 23:23:42 -0500

    WIP: NAMD log parser and thread

--- base/namdlogparser.cpp
+++ base/namdlogparser.cpp
@@ -1,19 +1,14 @@
 #include "namdlogparser.h"
 
-NAMDLog::NAMDLog()
-{
+NAMDLog::NAMDLog() {}
 
-}
-
-void NAMDLog::clearData()
-{
+void NAMDLog::clearData() {
   mEnergyTitle.clear();
   mEnergyData.clear();
   mPairData.clear();
 }
 
-void NAMDLog::readFromStream(QTextStream &ifs)
-{
+void NAMDLog::readFromStream(QTextStream &ifs) {
   QString line;
   bool firsttime = true;
   while (!ifs.atEnd()) {
@@ -21,7 +16,8 @@ void NAMDLog::readFromStream(QTextStream &ifs)
     if (firsttime) {
       if (line.startsWith("ETITLE:")) {
         firsttime = false;
-        QStringList titles = line.split(QRegExp("(ETITLE:|\\s+)"), Qt::SkipEmptyParts);
+        QStringList titles =
+            line.split(QRegExp("(ETITLE:|\\s+)"), Qt::SkipEmptyParts);
         for (auto it = titles.begin(); it != titles.end(); ++it) {
           // +1 to skip the "ETITLE" field
           const auto keyFound = mEnergyData.find(*it);
@@ -33,54 +29,101 @@ void NAMDLog::readFromStream(QTextStream &ifs)
       }
     }
     if (line.startsWith("ENERGY:")) {
-      QStringList fields = line.split(QRegExp("[A-Z:\\s]+"), Qt::SkipEmptyParts);
+      QStringList fields =
+          line.split(QRegExp("[A-Z:\\s]+"), Qt::SkipEmptyParts);
       for (int i = 0; i < mEnergyTitle.size(); ++i) {
         mEnergyData[mEnergyTitle[i]].append(fields[i].toDouble());
       }
     }
     if (line.startsWith("PAIR INTERACTION:")) {
-      QStringList fields = line.split(QRegExp("[A-Z:_\\s]+"), Qt::SkipEmptyParts);
+      QStringList fields =
+          line.split(QRegExp("[A-Z:_\\s]+"), Qt::SkipEmptyParts);
       if (fields.size() == 7) {
-        mPairData["VDW_FORCE"].append(QVector3D(fields[1].toDouble(), fields[2].toDouble(), fields[3].toDouble()));
-        mPairData["ELECT_FORCE"].append(QVector3D(fields[4].toDouble(), fields[5].toDouble(), fields[6].toDouble()));
+        mPairData["VDW_FORCE"].append(QVector3D(
+            fields[1].toDouble(), fields[2].toDouble(), fields[3].toDouble()));
+        mPairData["ELECT_FORCE"].append(QVector3D(
+            fields[4].toDouble(), fields[5].toDouble(), fields[6].toDouble()));
       }
     }
   }
 }
 
-QVector<double> NAMDLog::getStep() const
-{
-  return getEnergyData("TS");
-}
+QVector<double> NAMDLog::getStep() const { return getEnergyData("TS"); }
 
-QVector<double> NAMDLog::getVdW() const
-{
-  return getEnergyData("VDW");
-}
+QVector<double> NAMDLog::getVdW() const { return getEnergyData("VDW"); }
 
-QVector<double> NAMDLog::getElectrostatic() const
-{
+QVector<double> NAMDLog::getElectrostatic() const {
   return getEnergyData("ELECT");
 }
 
-QVector<double> NAMDLog::getEnergyData(const QString &title, bool *ok) const
-{
+QVector<double> NAMDLog::getEnergyData(const QString &title, bool *ok) const {
   const auto keyFound = mEnergyData.find(title);
   if (keyFound == mEnergyData.end()) {
-    if (ok != nullptr) *ok = false;
+    if (ok != nullptr)
+      *ok = false;
     return QVector<double>();
   } else {
-    if (ok != nullptr) *ok = true;
+    if (ok != nullptr)
+      *ok = true;
     return mEnergyData.value(title);
   }
 }
 
-QVector<ForceType> NAMDLog::getVdWForce() const
-{
+QVector<ForceType> NAMDLog::getVdWForce() const {
   return mPairData.value("VDW_FORCE");
 }
 
-QVector<ForceType> NAMDLog::getElectrostaticForce() const
-{
+QVector<ForceType> NAMDLog::getElectrostaticForce() const {
   return mPairData.value("ELECT_FORCE");
+}
+
+doBinning::doBinning(HistogramScalar<double> &histogram,
+                     HistogramScalar<size_t> &count, const QVector<int> &column)
+    : mHistogram(histogram), mCount(count), mColumn(column) {}
+
+void doBinning::operator()(const QVector<double> &fields, double energy) {
+  // get the position of current point from trajectory
+  QVector<double> pos(mHistogram.dimension());
+  for (int i = 0; i < pos.size(); ++i) {
+    pos[i] = fields[mColumn[i]];
+  }
+  bool inBoundary = false;
+  const size_t addr = mHistogram.address(pos, &inBoundary);
+  if (inBoundary) {
+    mHistogram[addr] += energy;
+    ++mCount[addr];
+  }
+}
+
+ParsePairInteractionThread::ParsePairInteractionThread(QObject *parent)
+  : QThread(parent) {}
+
+ParsePairInteractionThread::~ParsePairInteractionThread()
+{
+  // am I doing the right things?
+  qDebug() << Q_FUNC_INFO;
+  mutex.lock();
+  mutex.unlock();
+  wait();
+  quit();
+}
+
+void ParsePairInteractionThread::invokeThread(const QString &logFileName,
+                                              const QString &title,
+                                              const QString &trajectoryFileName, const QVector<Axis> &ax,
+                                              const QVector<int> &column) {
+  qDebug() << Q_FUNC_INFO;
+  QMutexLocker locker(&mutex);
+  mLogFileName = logFileName;
+  mTitle = title;
+  mTrajectoryFileName = trajectoryFileName;
+  mAxis = ax;
+  mColumn = column;
+  if (!isRunning()) {
+    start(LowPriority);
+  }
+}
+
+void ParsePairInteractionThread::run() {
+  // TODO
 }
--- base/namdlogparser.h
+++ base/namdlogparser.h
@@ -1,30 +1,73 @@
 #ifndef NAMDLOGPARSER_H
 #define NAMDLOGPARSER_H
 
+#include "base/histogram.h"
+
 #include <QMap>
-#include <QVector>
-#include <QVector3D>
+#include <QMutex>
 #include <QTextStream>
+#include <QThread>
+#include <QVector3D>
+#include <QVector>
 #include <cstring>
 
 using ForceType = QVector3D;
 
-class NAMDLog
-{
+class NAMDLog {
 public:
   NAMDLog();
   void clearData();
-  void readFromStream(QTextStream& ifs);
+  void readFromStream(QTextStream &ifs);
   QVector<double> getStep() const;
   QVector<double> getVdW() const;
   QVector<double> getElectrostatic() const;
-  QVector<double> getEnergyData(const QString& title, bool* ok = nullptr) const;
+  QVector<double> getEnergyData(const QString &title, bool *ok = nullptr) const;
   QVector<ForceType> getVdWForce() const;
   QVector<ForceType> getElectrostaticForce() const;
+
 private:
   QStringList mEnergyTitle;
   QMap<QString, QVector<double>> mEnergyData;
   QMap<QString, QVector<ForceType>> mPairData;
+};
+
+struct doBinning {
+public:
+  doBinning(HistogramScalar<double> &histogram, HistogramScalar<size_t> &count,
+            const QVector<int> &column);
+  void operator()(const QVector<double> &fields, double energy);
+  HistogramScalar<double> &mHistogram;
+  HistogramScalar<size_t> &mCount;
+  const QVector<int> mColumn;
+};
+
+class ParsePairInteractionThread : public QThread {
+  Q_OBJECT
+public:
+  ParsePairInteractionThread(QObject *parent = nullptr);
+  ~ParsePairInteractionThread();
+  void invokeThread(const QString &logFileName, const QString &title,
+                    const QString &trajectoryFileName,
+                    const QVector<Axis>& ax,
+                    const QVector<int> &column);
+
+signals:
+  void error(QString err);
+  void done();
+  void doneHistogram(HistogramScalar<double> histogram);
+  void doneLog(NAMDLog logObject);
+  void progress(QString stage, int percent);
+
+protected:
+  void run();
+
+private:
+  QMutex mutex;
+  QString mLogFileName;
+  QString mTitle;
+  QString mTrajectoryFileName;
+  QVector<Axis> mAxis;
+  QVector<int> mColumn;
 };
 
 #endif // NAMDLOGPARSER_H
